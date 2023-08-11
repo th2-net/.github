21c49718c930d8282789a0ca95e195fe48682622
andrey.shulika <andrey.shulika@exactprosystems.com>
2023-08-12 02:37:40 +0300

    TH2-1836

--- license-compliance/check_licenses.sh
+++ license-compliance/check_licenses.sh
@@ -7,15 +7,16 @@ fi
 
 #*******Params*******
 jq_path="jq"
-dir="licenses_check"
 cur_loc=`pwd`
+dir="$cur_loc/licenses_check"
+rm -rf $dir
 mkdir -p $dir
-#rm -rf $dir
-lic_file="$cur_loc/$dir/licenses.json"
+lic_file="$dir/licenses.json"
 
 link_allowed_licenses="https://raw.githubusercontent.com/th2-net/.github/th2-1836-json-files-update/license-compliance/gradle-license-report/allowed-licenses.json"
 link_license_normalizer_bundle="https://raw.githubusercontent.com/th2-net/.github/th2-1836-json-files-update/license-compliance/gradle-license-report/license-normalizer-bundle.json"
 allowed_licenses="$dir/allowed-licenses.json"
+normalizer_file="$dir/license-normalizer-bundle.json"
 unknown_license=""
 warnings="$dir/warnings.csv"
 
@@ -30,6 +31,11 @@ echo $head > $final_report
 
 convert_allow_lic="$dir/convert_allow_lic.csv"
 convert_bom_lic="$dir/convert_bom_lic.csv"
+
+project_name=`cat .git/config | grep "url = git@github.com:th2-net" | awk -F '/' '{print $2}' | sed 's/.git//g'`
+branch=`cat .git/HEAD | awk -F '/' '{print $3}'`
+echo "Project name = $project_name"
+echo "Branch name = $branch"
 #******end Params*****
 
 #Function to download file
@@ -52,10 +58,7 @@ normalize(){
 	local file=$1
 	cp $file $file"_bkp"
 	local regexp_file="$dir/regexp_file"
-	local normalizer_file="$dir/license-normalizer-bundle.json"
 	local reg_sed="$dir/sed_commands_list"
-	download_file "$link_license_normalizer_bundle" "$normalizer_file"
-
 	#Prepare regexp file format bundleName,regexp
 	$jq_path -r '.transformationRules[] | .lic = (.licenseNamePattern // .licenseUrlPattern) | [ .bundleName, .lic ] | @csv' $normalizer_file > $regexp_file
 
@@ -88,7 +91,11 @@ normalize(){
 #	rm $reg_sed $regexp_file
 }
 
+#Download allowed-licenses.json
+download_file "$link_allowed_licenses" "$allowed_licenses"
 
+#Download license-normalizer-bundle.json
+download_file "$link_license_normalizer_bundle" "$normalizer_file"
 
 
 #Define mode and reformat files before checking
@@ -122,29 +129,14 @@ case $1 in
 	;;
 esac
 
-#*******Params*******
-
-link_allowed_licenses="https://raw.githubusercontent.com/th2-net/.github/th2-1836-json-files-update/license-compliance/gradle-license-report/allowed-licenses.json"
-allowed_licenses="$dir/allowed-licenses.json"
-unknown_license=""
-
-temp_res="$dir/temp_res"
-res_lic="$dir/res.lic"
-report_before_check="$dir/report.csv_before_check"
-failed_licenses="$dir/failed_licenses.csv"
-passed_licenses="$dir/passed_licenses.csv"
-final_report="$dir/licenses_report.csv"
-head="Testing tool name,Open Source Code Library Name (contained within the Testing Tool),Version,Associated Open Source License,Comment"
-echo $head > $final_report
-
-convert_allow_lic="$dir/convert_allow_lic.csv"
-convert_bom_lic="$dir/convert_bom_lic.csv"
-#******end Params*****
 
 echo "Parsing file $lic_file"
 
 #Make report prior to check
-$jq_path -r '.dependencies[] | .licenses = try (.moduleLicenses[].moduleLicense) catch "null" | [.moduleName, .moduleVersion, .licenses ] | @csv' $lic_file | sort -u > $temp_res
+#Old version without URLs
+#$jq_path -r '.dependencies[] | .licenses = try (.moduleLicenses[].moduleLicense) catch "null" | [.moduleName, .moduleVersion, .licenses ] | @csv' $lic_file | sort -u > $temp_res
+
+$jq_path -r '.dependencies[] | [.moduleName, .moduleVersion, try (.moduleLicenses[].moduleLicense) catch "null", try (.moduleLicenses[].moduleLicenseUrl) catch "null"] | @csv' $lic_file | sort -u > $temp_res 
 
 echo "Forming report"
 
@@ -153,53 +145,91 @@ for main in `cat $temp_res | tr ' ' ';'`
         name=`echo $main | awk -F "," '{print $1}'`
         version=`echo $main | awk -F "," '{print $2}'`
         lic=""
+	lic_url=""
 	#echo "Head = $name , $version"
+
+	#Concatenate strings if licenses more than one via vertical line |
         for line in `cat $temp_res | tr ' ' ';' | grep $name`
             do
 		#echo "Line = $line"
-		if [[ "$lic" == "" ]]
+		column_number=`echo $line | awk -F "," '{print NF}'`
+		#echo "Column number = $column_number"
+		if [ "$column_number" -gt 4 ]
 		then
-			lic=`echo $line | awk -F "," '{print $3}'`
+			for (( col=3; col < $column_number+1; col++ ))
+			do
+				#echo "Col = $col"
+				item=`echo $line | awk -v num=$col -F "," '{print $num}'`
+				reg_exp="^[\"|;]?http" #to find links starts from http
+				#echo "Reg = $reg_exp"
+				#echo "item = $item"
+				if [[ "$item" =~ $reg_exp ]]
+				then
+					if [[ $lic_url == "" ]]
+					then
+						lic_url=$item
+					else
+						lic_url=$lic_url" | "$item
+					fi
+				else
+					if [[ $lic == "" ]]
+					then
+						lic=$item
+					else
+						lic=$lic" | "$item
+					fi
+				fi
+			done
 		else
-	                lic=$lic" | "`echo $line | awk -F "," '{print $3}'`
+			if [[ "$lic" == "" ]]
+			then
+				lic=`echo $line | awk -F "," '{print $3}'`
+				lic_url=`echo $line | awk -F "," '{print $4}'`
+			else
+		                lic=$lic" | "`echo $line | awk -F "," '{print $3}'`
+				lic_url=$lic_url" | "`echo $line | awk -F "," '{print $4}'`
+			fi
 		fi
-		#echo "Lic = $lic"
+#		echo "Lic = $lic"
+#		echo "Lic url = $lic_url"
+#		echo "***************************"
             done
+
+#	echo "Head = $name , $version"
+#       echo "Lic = $lic"
+#       echo "Lic url = $lic_url"
+#	echo "***************************"
 	lic=`echo $lic | sed 's/"//g' | sed 's/^/"/g' | sed 's/$/"/g'`
-        echo $name,$version,$lic | tr ';' ' ' >> $res_lic
+	lic_url=`echo $lic_url | sed 's/"//g' | sed 's/^/"/g' | sed 's/$/"/g'`
+        echo "$name,$version,$lic,$lic_url" | tr ';' ' ' >> $res_lic
 	lic=""
+	lic_url=""
 	#echo "-----------------------"
     done
 
 cat $res_lic | sort -u > $report_before_check
 
-rm $res_lic $temp_res
+#rm $res_lic $temp_res
 echo "Forming completed"
 
 
-#Download allowed-licenses.json
-echo "Download allowed-licenses file"
-download_file "$link_allowed_licenses" "$allowed_licenses"
-
-
-#simple text
-#Reformat allowed-licenses
-#/home/exp.exactpro.com/andrey.shulika/DEVOPS/work/soft/jq/jq-linux64 -r '.allowedLicenses[] | .moduleLicense' $allowed_licenses | sed 's/\^//g' | sed 's/\$//g' | sed 's/\\//g' | grep -v null | sort -u > $convert_allow_lic
-
-#regular expressions
+#regular expressions from allowed-licenses.json
 $jq_path -r '.allowedLicenses[] | .moduleLicense' $allowed_licenses | grep -v null | sort -u > $convert_allow_lic
 
+#bom lines from allowed-licenses.json
 $jq_path -r '.allowedLicenses[] | .licenses = try (.mvnRepositoryLicense) catch "null" | [.moduleName, .moduleVersion, .licenses ] | @csv' $allowed_licenses | sort -u | grep -v ",," | sed 's/"//g' > $convert_bom_lic
 
 
 #Check license based on regular expressions in allowed-licenses.json file
 echo "Checking licenses"
+echo "--------------------------------------"
 
-while IFS=, read -r name version license
+while IFS=, read -r name version license url
 do
 	#echo "Name = $name"
 	#echo "Version = $version"
 	#echo "License = $license"
+	#echo "Url = $lic_url"
 	known_license=0 #0 = false
 	mod_license=`echo $license | sed 's/"//g'`
 
@@ -240,13 +270,15 @@ do
         if [ "$known_license" -eq 0 ]; then
                echo "Line = $name,$version,$license"
                echo "Result = ***FAILED***"
-               echo "$name,$version,$license" >> $failed_licenses
+	       echo "Lic = $license"
+	       echo "Url = $url"
+               echo "\"$project_name/$branch\",$name,$version,$license,$url" >> $failed_licenses
                echo "--------------------------------------"
         else
-               echo "$name,$version,$license" >> $passed_licenses
+               echo "\"$project_name/$branch\",$name,$version,$license,$url" >> $passed_licenses
 	       #echo "Result = *PASSED*"
         fi
-	`echo ",$name,$version,$license" >> $final_report`
+	`echo "\"$project_name/$branch\",$name,$version,$license,$url" >> $final_report`
 	#echo "--------------------------------------------------------------"
 
 done < $report_before_check
@@ -256,40 +288,3 @@ echo "Folder = $dir"
 echo "Passed report = $passed_licenses"
 echo "Failed report = $failed_licenses"
 echo "Final report = $final_report"
-
-
-
-
-#Old code - comparing based on text
-#Check license
-#echo "Checking licenses"
-#for line in `cat $report_before_check | tr ' ' ';'`
-#do
-#	item=`echo $line | awk -F "," '{print $3}' | tr ';' ' ' | sed 's/"//g'`
-#	known_license=0 #0 = false
-#	#echo "Item = $item"
-#
-#	if [[ "$item" != "null" ]]; then
-#		for lic_line in `cat $allowed_licenses | awk -F "," '{print $1}' | tr ' ' ';'`
-#		do
-#			#echo "License line = $lic_line"
-#			lic="`echo $lic_line | tr ';' ' ' | sed 's/ | /|/g'`"
-#			#echo "Lic = $lic"
-#			if [[ "$item" == "$lic" ]]; then
-#				known_license=1 # 1 - true
-#				#echo "Result = PASSED"
-#				break;
-#			fi
-#		done
-#	fi
-#
-#	if [ "$known_license" -eq 0 ]; then
-#		echo "Line = $line "
-#		echo "Result = ***FAILED***"
-#		`echo "$line" | tr ';' ' ' >> $failed_licenses`
-#		echo "-----------------------"
-#	else
-#		`echo "$line" | tr ';' ' ' >> $passed_licenses`
-#	fi
-#done
-#end old code
