60a4efa9102b75d0344bb2c30fc516b86fffff41
Dark <75368248+Andrei-Shulika@users.noreply.github.com>
2024-05-15 14:27:51 +0200

    Th2 1836 json files update (#33)
    * normalizer update
    
    * adding boms to the list
    
    * remove simple BSD items
    
    * adding list of allowed licenses
    
    * corrections accroding to comments except sort
    
    * version regular added to exp libraries/boms
    
    * adding apche and bsd clause together
    
    * [th2-1836] reformat code
    
    * adding new property licenseCategory, initial Permissive for all
    
    * revert changes
    
    * license category added
    
    * initial script to check licenses
    
    * latest version
    
    * Refactored gradle license report configs
    * removed duplicates
    
    * jq path variable added
    
    * new version
    
    * adding cur location param
    
    * fix path to jq
    
    * adding bsd2 and cc0
    
    * adding 2 other instances
    
    * Update license-normalizer-bundle.json
    
    * Changed '|' to 'OR' for multi-license
    
    * TH2-1836
    
    * lic category added
    
    * new version
    
    * new version
    
    * add license check execution
    
    * license check workflow
    
    * add allowed combinations
    
    * change | to OR
    
    * add allowed licenses
    
    * escape plus
    
    * fix count
    
    * new items
    
    * added org.jetbrains.kotlin:kotlin-stdlib-common
    
    * fix typo
    
    * corrections to MPL naming
    
    * additions
    
    * add regexp for error prone google library
    
    * due to the dependency-license-report plugin  version changes info file added and category removed from normalizer file
    
    * change to work with the new info file and to merge into main branch
    
    * refresh license_check to the latest version
    
    * remove transformation rules and modify workflow to take input parameters
    
    * change to strict Apache2.0 license according to the Nikita's request
    
    ---------
    
    Co-authored-by: andrey.shulika <andrey.shulika@exactprosystems.com>
    Co-authored-by: nikita.smirnov <nikita.smirnov@exactprosystems.com>
    Co-authored-by: sabamushtashvili <94606782+sabamushtashvili@users.noreply.github.com>

--- license-compliance/README.md
+++ license-compliance/README.md
@@ -1,3 +1,4 @@
 # Restricted Licenses List
+* Please keep in mind all records are case sensitive
 * This file is used by license-compliance action.
 * To add license to list, please put its name on new line. If the name consists spaces, please wrap one into single quotes (''). 
---
+++ license-compliance/check_licenses.sh
@@ -0,0 +1,318 @@
+#!/bin/bash
+#Designed by LLC Exactprosystems 
+#24.10.2023
+#DevOps Engineer
+#Andrei Shulika
+#andrey.shulika@exactprosystems.com
+
+log_date_time(){
+	local dt=`date '+%Y-%m-%d %H:%M:%S'`
+	echo ${dt}
+}
+
+if [ -z "$1" ];then
+	echo "`log_date_time`: ERROR: Param 1 is empty - which mode to use? (java or python)"
+	exit 2
+fi
+
+#*******Params*******
+jq_path="jq"
+cur_loc=`pwd`
+dir="$cur_loc/licenses_check"
+rm -rf $dir
+mkdir -p $dir
+lic_file="$dir/licenses.json"
+
+link_allowed_licenses="https://raw.githubusercontent.com/th2-net/.github/main/license-compliance/gradle-license-report/allowed-licenses.json"
+link_license_normalizer_bundle="https://raw.githubusercontent.com/th2-net/.github/main/license-compliance/gradle-license-report/license-normalizer-bundle.json"
+link_info_licenses="https://raw.githubusercontent.com/th2-net/.github/main/license-compliance/gradle-license-report/info-licenses.json"
+
+allowed_licenses="$dir/allowed-licenses.json"
+normalizer_file="$dir/license-normalizer-bundle.json"
+info_licenses_file="$dir/info-licenses.json"
+unknown_license=""
+warnings="$dir/warnings.csv"
+
+temp_res="$dir/temp_res"
+res_lic="$dir/res.lic"
+report_before_check="$dir/report.csv_before_check"
+failed_licenses="$dir/failed_licenses.csv"
+touch $failed_licenses
+passed_licenses="$dir/passed_licenses.csv"
+final_report="$dir/licenses_report.csv"
+head="Testing tool name,Open Source Code Library Name (contained within the Testing Tool),Version,Associated Open Source License,Comment"
+echo $head > $final_report
+
+convert_allow_lic="$dir/convert_allow_lic.csv"
+convert_bom_lic="$dir/convert_bom_lic.csv"
+
+project_name=`cat .git/config | grep "url = git@" | sed 's/\.git$//g' | awk -F "/" '{print $NF}'`
+#branch=`cat .git/HEAD | awk -F '/' '{print $3}'`
+branch=`git branch 2> /dev/null | egrep "\* " | sed -e 's/^* //g' | awk '{print $NF}' | sed 's/)//g'`
+echo "*****************************"
+echo "`log_date_time`: Project name = $project_name"
+echo "`log_date_time`: Branch name = $branch"
+echo "*****************************"
+#******end Params*****
+
+#Function to download file
+download_file(){
+	local link=$1
+	local output_file=$2
+	echo "`log_date_time`: Download file from $link"
+	wget -q -O $output_file $link
+	if [ $? -eq 0 ]; then
+	        echo "`log_date_time`: Download - Completed"
+	else
+	        echo "`log_date_time`: ERROR: download file problem"
+	        echo "`log_date_time`: Link = $link"
+	        exit 2
+	fi
+}
+
+#Function to normalize license names
+normalize(){
+	local file=$1
+	cp $file $file"_bkp"
+	local regexp_file="$dir/regexp_file"
+	local reg_sed="$dir/sed_commands_list"
+	#Prepare regexp file format bundleName,regexp
+	$jq_path -r '.transformationRules[] | .lic = (.licenseNamePattern // .licenseUrlPattern) | [ .bundleName, .lic ] | @csv' $normalizer_file > $regexp_file
+
+	#Prepare sed commands to apply to initial file to replace known pattern to normal license name
+	while IFS= read -r line
+	do
+		local bundleName=`echo $line | awk -F "\",\"" '{print $1}' | sed 's/"//g'`
+		local exp=`echo $line | awk -F "\",\"" '{print $2}' | sed 's/"//g'`
+		local mod_exp=`echo $exp | sed 's/\\\//g' | sed 's/\^/"/g' | sed 's/\\$/"/g'`
+		#Find lic name that need to be applied instead of text
+		local lic=`$jq_path -r '.bundles[] | select (.bundleName == "'$bundleName'") | .licenseName' $normalizer_file | sed 's/^/"/g' | sed 's/$/"/g'`
+		#echo "Exp = $exp"
+		#echo "Mod_exp = $mod_exp"
+                #echo "BundleName = $bundleName"
+		#echo "Lic = $lic"
+		if  [[  -z $lic  ]]; then
+			echo "*********************************************"
+			echo "`log_date_time`: Warning"
+		        echo "`log_date_time`: YOU HAVE UNDEFINED TRANSFORMATION RULE in $normalizer_file"
+			echo "`log_date_time`: Regexp = $exp"
+			echo "`log_date_time`: Bundle name = $bundleName"
+			echo "`log_date_time`: The rule is skipped."
+			echo "Undefined transformation rule,bundleName=$bundleName,regexp=$exp" >> $warnings
+			echo "*********************************************"
+		else
+			echo "s#${mod_exp}#${lic}#g" >> $reg_sed
+		fi
+	done < $regexp_file
+	#Apply sed commands replace patterns
+	sed -f $reg_sed -i $file
+#	rm $reg_sed $regexp_file
+}
+
+#Download allowed-licenses.json
+download_file "$link_allowed_licenses" "$allowed_licenses"
+
+#Download license-normalizer-bundle.json
+download_file "$link_license_normalizer_bundle" "$normalizer_file"
+
+#Download info-licenses.json
+download_file "$link_info_licenses" "$info_licenses_file"
+
+#Define mode and reformat files before checking
+case $1 in
+
+	java|JAVA|Java)
+	echo "`log_date_time`: Using java mode"
+	echo "`log_date_time`: Running gradle plugins"
+	./gradlew checkLicense generateLicenseReport
+	cp build/reports/dependency-license/licenses.json $lic_file
+	echo "`log_date_time`: Running plugins - completed"
+	;;
+
+	python|PYTHON|Python)
+	echo "`log_date_time`: Using python mode"
+	echo "`log_date_time`: Running pip-licenses"
+	pip install pip-licenses
+	pip-licenses --format=json --output-file=pyth_licenses.json
+	#Reformat to common
+	cat pyth_licenses.json | sed 's/^\[/\{ "dependencies": \[/g' | sed 's/^\]/\] \}/g' > p_temp
+	cat p_temp | sed 's/"Name"/"moduleName"/g' | sed 's/"Version"/"moduleVersion"/g' | sed 's/\("License":\)\( ".*"\)/"moduleLicenses": \[ \{ "moduleLicense": \2 \} \]/g' > pyth_licenses.json
+	rm p_temp
+	mv pyth_licenses.json $lic_file
+	normalize "$lic_file"
+	echo "`log_date_time`: Running pip-licenses - completed"
+	;;
+
+	*)
+	echo "`log_date_time`: Unknown mode"
+	exit 2
+	;;
+esac
+
+
+echo "`log_date_time`: Parsing file $lic_file"
+
+#Make report prior to check
+#Old version without URLs
+#$jq_path -r '.dependencies[] | .licenses = try (.moduleLicenses[].moduleLicense) catch "null" | [.moduleName, .moduleVersion, .licenses ] | @csv' $lic_file | sort -u > $temp_res
+
+$jq_path -r '.dependencies[] | [.moduleName, .moduleVersion, try (.moduleLicenses[].moduleLicense) catch "null", try (.moduleLicenses[].moduleLicenseUrl) catch "null"] | @csv' $lic_file | sort -u > $temp_res 
+
+echo "`log_date_time`: Forming report"
+
+for main in `cat $temp_res | tr ' ' ';'`
+    do
+        name=`echo $main | awk -F "," '{print $1}'`
+        version=`echo $main | awk -F "," '{print $2}'`
+        lic=""
+	lic_url=""
+	#echo "Head = $name , $version"
+
+	#Concatenate strings if licenses more than one via vertical line |
+        for line in `cat $temp_res | tr ' ' ';' | grep $name`
+            do
+		#echo "Line = $line"
+		column_number=`echo $line | awk -F "," '{print NF}'`
+		#echo "Column number = $column_number"
+		if [ "$column_number" -gt 4 ]
+		then
+			for (( col=3; col < $column_number+1; col++ ))
+			do
+				#echo "Col = $col"
+				item=`echo $line | awk -v num=$col -F "," '{print $num}'`
+				reg_exp="^[\"|;]?http" #to find links starts from http
+				#echo "Reg = $reg_exp"
+				#echo "item = $item"
+				if [[ "$item" =~ $reg_exp ]]
+				then
+					if [[ $lic_url == "" ]]
+					then
+						lic_url=$item
+					else
+						lic_url=$lic_url" | "$item
+					fi
+				else
+					if [[ $lic == "" ]]
+					then
+						lic=$item
+					else
+						lic=$lic" | "$item
+					fi
+				fi
+			done
+		else
+			if [[ "$lic" == "" ]]
+			then
+				lic=`echo $line | awk -F "," '{print $3}'`
+				lic_url=`echo $line | awk -F "," '{print $4}'`
+			else
+		                lic=$lic" | "`echo $line | awk -F "," '{print $3}'`
+				lic_url=$lic_url" | "`echo $line | awk -F "," '{print $4}'`
+			fi
+		fi
+#		echo "Lic = $lic"
+#		echo "Lic url = $lic_url"
+#		echo "***************************"
+            done
+
+#	echo "Head = $name , $version"
+#       echo "Lic = $lic"
+#       echo "Lic url = $lic_url"
+#	echo "***************************"
+	lic=`echo $lic | sed 's/"//g' | sed 's/^/"/g' | sed 's/$/"/g'`
+	lic_url=`echo $lic_url | sed 's/"//g' | sed 's/^/"/g' | sed 's/$/"/g'`
+        echo "$name,$version,$lic,$lic_url" | tr ';' ' ' >> $res_lic
+	lic=""
+	lic_url=""
+	#echo "-----------------------"
+    done
+
+cat $res_lic | sort -u > $report_before_check
+
+#rm $res_lic $temp_res
+echo "`log_date_time`: Forming completed"
+
+
+#regular expressions from allowed-licenses.json
+$jq_path -r '.allowedLicenses[] | .moduleLicense' $allowed_licenses | grep -v null | sort -u > $convert_allow_lic
+
+#bom lines from allowed-licenses.json
+$jq_path -r '.allowedLicenses[] | .licenses = try (.mvnRepositoryLicense) catch "null" | [.moduleName, .moduleVersion, .licenses ] | @csv' $allowed_licenses | sort -u | grep -v ",," | sed 's/"//g' > $convert_bom_lic
+
+
+#Check license based on regular expressions in allowed-licenses.json file
+echo "`log_date_time`: Checking licenses"
+echo "--------------------------------------"
+
+while IFS=, read -r name version license url
+do
+	lic_category=""
+	known_license=0 #0 = false
+        ORchange=`echo $license | sed 's/"//g'`
+        mod_license=`echo $ORchange | sed 's/ | / OR /g'`
+
+	if [[ "$mod_license" != "null" ]]; then
+		while IFS= read -r exp
+		do
+		exp=`echo $exp | sed 's/|/\\\|/g'` #escape vertical line in expression
+		#echo "Exp = $exp"
+		if [[ $mod_license =~ $exp ]]; then
+		  #echo "Passed"
+		  known_license=1 #1 = true
+		  break;
+		fi
+		done < $convert_allow_lic
+	else # if license == null
+		mod_name=`echo $name | sed 's/"//g'`
+		mod_ver=`echo $version | sed 's/"//g'`
+		#echo "Mod_name = $mod_name"
+		#echo "Mod_ver = $mod_ver"
+		while IFS=, read -r c_name c_ver c_lic
+		do
+			#echo "C_name = $c_name"
+			c_ver=`echo $c_ver | sed 's/\\\d/[0-9]/g'` #replace \d to [0-9] in regular expressions to apply in bash according to POSIX
+			#echo "C_ver = $c_ver"
+			#echo "C_lic = $c_lic"
+
+			if [[ $mod_name =~ $c_name ]]; then
+				if [[ $mod_ver =~ $c_ver ]]; then
+					known_license=1
+					license=`echo $c_lic | sed 's/^/"/g' | sed 's/$/"/g'`
+					break;
+				fi
+			fi
+		done < $convert_bom_lic
+	fi
+
+
+	#Getting info about license category
+	lic_category=`$jq_path --argjson n "$license" '.bundles[] | select (.licenseName == $n) | .licenseCategory' $info_licenses_file`
+
+	#echo "Project name = $project_name"
+	#echo "Branch = $branch"
+        #echo "Name = $name"
+        #echo "Version = $version"
+        #echo "License = $license"
+        #echo "Url = $url"
+        #echo "licenseCategory = $lic_category"
+
+        if [ "$known_license" -eq 0 ]; then
+               echo "`log_date_time`: Line = $name,$version,$license"
+               echo "`log_date_time`: Result = ***FAILED***"
+               echo "\"$project_name/$branch\",$name,$version,$license,$url,$lic_category" >> $failed_licenses
+               echo "--------------------------------------"
+        else
+               echo "\"$project_name/$branch\",$name,$version,$license,$url,$lic_category" >> $passed_licenses
+	       #echo "Result = *PASSED*"
+        fi
+	echo "\"$project_name/$branch\",$name,$version,$license,$url,$lic_category" >> $final_report
+	#echo "--------------------------------------------------------------"
+
+done < $report_before_check
+
+echo "`log_date_time`: CHECK LICENSES - COMPLETED. PLEASE SEE REPORTS WITH DETAILS."
+echo "Folder = $dir"
+echo "Passed report = $passed_licenses"
+echo "Failed report = $failed_licenses"
+echo "Final report = $final_report"
+
